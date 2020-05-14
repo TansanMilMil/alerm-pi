73e9d2857e2741d950afa68edaa165ec6cf6a78c
tansantktk <tansantktk@hotmail.co.jp>
2020-05-14 21:14:50 +0900

    catch exception

--- alerm-pi.py
+++ alerm-pi.py
@@ -1,7 +1,18 @@
 import subprocess
+import datetime
+import traceback
 
-# ring alerm.
-subprocess.run('cd /home/pi/alerm-pi', shell=True)
-subprocess.run('sudo amixer ALSA cset numid=3 1', shell=True)
-subprocess.run('mpg321 alerm.mp3', shell=True)
+try:
+    # ring alerm.
+    subprocess.run('cd /home/pi/alerm-pi', shell=True)
+    subprocess.run('sudo amixer ALSA cset numid=3 1', shell=True)
+    subprocess.run('mpg321 alerm.mp3', shell=True)
+except:
+    e = traceback.format_exc()
+    print(e)
+    dt_now = datetime.datetime.now()
+    path = './alerm-pi-error-log.txt'
+    with open(path, mode='a') as f:
+        f.write(dt_now.strftime('- Exception - %Y/%m/%d %H:%M:%S \n'))
+        f.write(e + '\n\n')
 
--- news-pi.py
+++ news-pi.py
@@ -2,33 +2,49 @@ import subprocess
 import datetime
 import sys
 from newsapi import NewsApiClient
+import traceback
+from mutagen.mp3 import MP3 as mp3
+import pygame
+import time
 
-# get headline
-newsapi = NewsApiClient(api_key='671c92cd456f4d639703ea2e24671bbf')
-top_headline = newsapi.get_top_headlines(country='jp')
-if len(top_headline['articles']) > 5:
-	top_headline['articles'] = top_headline['articles'][0:5]
+try:
+    # get headline
+    newsapi = NewsApiClient(api_key='671c92cd456f4d639703ea2e24671bbf')
+    top_headline = newsapi.get_top_headlines(country='jp')
+    
+    if len(top_headline['articles']) > 5:
+        top_headline['articles'] = top_headline['articles'][0:5]
+    
+    # start music
+    #subprocess.run('sudo amixer ALSA cset numid=3 1', shell=True)
+    #subprocess.run('mpg321 ./bgm.mp3 &', shell=True)
+    pygame.mixer.init()
+    pygame.mixer.music.load('./bgm.mp3')
+    pygame.mixer.music.play(1)
 
-# start music
-#subprocess.run('mpg321 ./bgm.mp3 &', shell=True)
-
-# read news
-dt_now = datetime.datetime.now()
-subprocess.run('./jsay.sh ' + str(dt_now.month) + '月' + str(dt_now.day) + '日のニュースをお伝えします。'
-		, shell=True)
-
-for news in top_headline['articles']:
-	news['title'] = """ ' """ + news['title'] + """ ' """
-	print(news['title'])
-	subprocess.run('./jsay.sh ' + news['title'], shell=True)
-
-	news['description'] = """ ' """ + news['description'] + """ ' """
-	print(news['description'])
-	# subprocess.run('./jsay.sh ' + news['description'], shell=True)
-
-	print('---------------')
-
-
-subprocess.run('./jsay.sh ' + '以上、ニュースをお伝えしました。', shell=True)
-
-sys.exit()
+    # read news
+    dt_now = datetime.datetime.now()
+    subprocess.run('./jsay.sh ' + str(dt_now.month) + '月' + str(dt_now.day) + '日のニュースをお伝えします。', shell=True)
+    
+    for news in top_headline['articles']:
+        news['title'] = """ ' """ + news['title'] + """ ' """
+        print(news['title'])
+        subprocess.run('./jsay.sh ' + news['title'], shell=True)
+            
+        news['description'] = """ ' """ + news['description'] + """ ' """
+        print(news['description'])
+        # subprocess.run('./jsay.sh ' + news['description'], shell=True)
+        
+        print('---------------')   
+    
+    subprocess.run('./jsay.sh ' + '以上、ニュースをお伝えしました。', shell=True)
+except:
+    e = traceback.format_exc()
+    print(e)
+    dt_now = datetime.datetime.now()
+    path = './news-pi-error-log.txt'
+    with open(path, mode='a') as f:
+        f.write(dt_now.strftime('- Exception - %Y/%m/%d %H:%M:%S \n'))
+        f.write(e + '\n\n')
+finally:
+    pygame.mixer.music.stop()
